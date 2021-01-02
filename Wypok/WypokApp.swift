6ce12f64b52e4fbb9c186e0a673e6a0610ffacfe
Robert Pelka <123455ff@gmail.com>
2021-01-02 22:24:32 +0100

    Initial Commit

---
+++ Wypok/WypokApp.swift
@@ -0,0 +1,17 @@
+//
+//  WypokApp.swift
+//  Wypok
+//
+//  Created by Robert Pelka on 02/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct WypokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
